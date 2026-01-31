b6c106655615e3264536c16f11866fc6c33a69f2
Maya Hamdy <info@ki-konzeptagentur.de>
2026-01-31 11:03:39 +0100

    feat function to add elements to canva

---
+++ js/game.js
@@ -0,0 +1,13 @@
+
+let canvas;
+let ctx;
+let charakter = new Image();
+
+
+function init () {
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    charakter.src = "../img_pollo_locco/img/2_character_pepe/2_walk/W-21.png";
+
+    ctx.drawImage(charakter, 20, 20, 50, 150);
+}
