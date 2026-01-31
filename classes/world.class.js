140b608f4a77b23cfc728e73348463f0bad1845b
Maya Hamdy <info@ki-konzeptagentur.de>
2026-01-31 14:25:35 +0100

    feat: add animationRequest

--- classes/world.class.js
+++ classes/world.class.js
@@ -1,32 +1,35 @@
 class World {
+  character = new Character();
 
+  enemies = [new Chicken(), new Chicken(), new Chicken()];
 
-character = new Character();
+  ctx;
 
-enemies = [
-    new Chicken(),
-    new Chicken(),
-    new Chicken(),
-];
+  constructor(canvas) {
+    this.ctx = canvas.getContext("2d");
+    this.draw();
+  }
 
-ctx;
-
-constructor(canvas){
-this.ctx = canvas.getContext("2d");
-this.draw();
-}
-
-draw() {
+  draw() {
     this.ctx.clearRect(0, 0, 720, 480);
 
     this.ctx.drawImage(
-        this.character.img,
-        this.character.x,
-        this.character.y,
-        this.character.width,
-        this.character.height
+      this.character.img,
+      this.character.x,
+      this.character.y,
+      this.character.width,
+      this.character.height,
     );
+    this.enemies.forEach((enemy) => {
+      this.ctx.drawImage(
+        enemy.img,
+        enemy.x,
+        enemy.y,
+        enemy.width,
+        enemy.height,
+      );
+    });
 
     requestAnimationFrame(() => this.draw());
-}
+  }
 }
