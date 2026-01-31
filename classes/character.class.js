e5298537494ba05c3a0711b9a4978196db111b39
Maya Hamdy <info@ki-konzeptagentur.de>
2026-01-31 13:14:09 +0100

    fix: changed class to upperletter

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,5 +1,10 @@
-class character extends movableObject {
+class Character extends movableObject {
  
+constructor() {
+    super().loadImage("img_pollo_locco/img/2_character_pepe/2_walk/W-21.png");
+}
+
+
 
 jump() {
     
--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -4,6 +4,12 @@ class movableObject {
     img;
 
 
+loadImage(path) {
+  this.img = new Image(); 
+  this.img.src = path; 
+}
+
+
     moveRight() {
 console.log("moving right");
     }
